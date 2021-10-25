25b3c2cea9b439efce620e77b8a659bc18ec51d2
alexkurasa <alexkurasa@gmail.com>
2021-10-25 18:33:31 +0300

    add not actual functional for photo element, next day have a delet this functional and create new page with photo

--- frontend/components/root/main-page/Gallery.vue
+++ frontend/components/root/main-page/Gallery.vue
@@ -1,13 +1,37 @@
 <template>
   <v-card class="mt-5 pa-1">
-    <div class="d-flex justify-space-between align-center">
-      <span class="ml-3"
+    <div class="d-flex flex-column justify-center align-start photo_gallery">
+      <span class="ml-3 d-flex justify-start"
         >Мои фотографии
         <span style="color: #cccccc">{{ this.user.gallery.length }}</span></span
       >
-      <span class="mr-4" style="color: #cccccc">Показать на карте</span>
+      <v-expansion-panels tile flat>
+        <v-expansion-panel>
+          <v-expansion-panel-header hide-actions @click="changeStatus()">
+            <div
+              class="d-flex justify-end align-end text_gallery"
+              style="color: #cccccc"
+            >
+              Показать на карте
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-row class="d-flex">
+              <v-col cols="3" v-for="item in this.user.gallery" :key="item.id">
+                <v-img
+                  height="100%"
+                  width="100%"
+                  :src="getStrapiMedia(item.url)"
+                  :lazy-src="getStrapiMedia(item.url)"
+                >
+                </v-img>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </div>
-    <v-row cols="10" class="mt-3 pa-0 ma-0" v-if="user">
+    <v-row cols="10" class="mt-3 pa-0 ma-0" v-if="user && !galleryStatus">
       <v-col cols="3" class="d-flex justify-center align-center mt-3">
         <v-file-input
           v-model="files"
@@ -20,12 +44,30 @@
         >
         </v-file-input>
       </v-col>
-      <v-col cols="3" v-for="item in this.user.gallery" :key="item.id">
+      <v-col cols="3">
         <v-img
           height="100%"
           width="100%"
-          :src="getStrapiMedia(item.url)"
-          :lazy-src="getStrapiMedia(item.url)"
+          :src="getStrapiMedia(this.user.gallery[1].url)"
+          :lazy-src="getStrapiMedia(this.user.gallery[1].url)"
+        >
+        </v-img>
+      </v-col>
+      <v-col cols="3">
+        <v-img
+          height="100%"
+          width="100%"
+          :src="getStrapiMedia(this.user.gallery[0].url)"
+          :lazy-src="getStrapiMedia(this.user.gallery[0].url)"
+        >
+        </v-img>
+      </v-col>
+      <v-col cols="3">
+        <v-img
+          height="100%"
+          width="100%"
+          :src="getStrapiMedia(this.user.gallery[2].url)"
+          :lazy-src="getStrapiMedia(this.user.gallery[2].url)"
         >
         </v-img>
       </v-col>
@@ -52,6 +94,12 @@ export default class Gallery extends Vue {
   image = ''
   imageUrl = ''
   files: File | null = null
+
+  galleryStatus: boolean = false
+  changeStatus() {
+    this.galleryStatus = !this.galleryStatus
+    console.log('now status', this.galleryStatus)
+  }
 
   test() {
     console.log(this.user.gallery)
